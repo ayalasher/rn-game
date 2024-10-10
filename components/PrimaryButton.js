5c541b5a1a68519372fc75ac7f08ecca08168f54
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-10-10 21:57:07 +0300

    Styled the buttons

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,15 +1,34 @@
-import { View, Text , Pressable} from "react-native"
+import { View, Text , Pressable, StyleSheet} from "react-native"
 
 export default function PrimaryButton(props) {
     const pressfunction = ()=>{
-        alert("Pressable pressed")
+        alert("Pressable pressed 😀")
     }
     return (
-        <Pressable>
-            <View onPress={pressfunction} >
-                <Text> {props.children} </Text>
+       
+            <View style={styles.btnoutercontainer} >
+                 <Pressable onPress={pressfunction} style={styles.btninnnercontainer} android_ripple={{color:"grey"}} >
+                    <Text style={styles.buttontext} > {props.children} </Text>
+                 </Pressable>
             </View>
-        </Pressable>
        
     )
 }
+
+const styles = StyleSheet.create({
+    btninnnercontainer:{
+        paddingVertical:8,
+        paddingHorizontal:16,
+        elevation:2,
+        backgroundColor:"white",
+    },
+    buttontext:{
+        color:"black",
+        textAlign:"center",
+    },
+    btnoutercontainer:{
+        borderRadius:10,
+        marginVertical:10,
+        overflow:"hidden"
+    }
+})
